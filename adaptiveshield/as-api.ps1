5a805d0cc998233b6c8c9ff198e7cbfdb858570d
mhanief <47133144+mhanief@users.noreply.github.com>
2023-09-15 16:41:01 +0800

    Add files via upload

---
+++ adaptiveshield/as-api.ps1
@@ -0,0 +1,24 @@
+﻿# Your Token details
+# This can be generated from Adaptive Shield dashboard
+# Steps to generate API key: User Profile > API > Generate new key
+$apiKey = "<your_api_key>"
+$accountId = "<your_account_id>"
+$baseUrl = "https://api.adaptive-shield.com/api/v1"
+
+# Include authorize in the header
+$headers = @{
+    "Authorization" = "Token $apiKey"
+}
+
+# Replace 'your-endpoint-here' with the specific API endpoint you want to access
+# $url = "$baseUrl/your-endpoint-here"
+# Can refer to this API document: https://adaptiveshield.document360.io/docs/api-reference 
+# To access the document, click you name > Knowledge Base. 
+$url = "$baseUrl/accounts/$accountId/user_inventory"
+
+# HTTP requests with GET 
+$response = Invoke-RestMethod -Uri $url -Headers $headers -Method Get
+
+# Work with the $response variable to access API data
+# For example, you can output the response to the console
+$response | ConvertTo-Json
